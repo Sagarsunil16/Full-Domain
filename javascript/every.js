1b5ca6e2c891db6566ba41ba5ca5033973eedcc3
Sagarsunil16 <155360655+Sagarsunil16@users.noreply.github.com>
2024-12-10 12:48:00 +0530

    array every and some methods

---
+++ javascript/every.js
@@ -0,0 +1,3 @@
+const arr = [22,22,34,66]
+const isEveryEVEN  = arr.every((elem)=>elem%2===0)
+console.log(isEveryEVEN)
---
+++ javascript/some.js
@@ -0,0 +1,4 @@
+//Purpose: Checks if at least one element satisfies a specified condition.
+const scores = [12,44,25,55,34,66]
+const hasPassed = scores.some((score)=>score>36)
+console.log(hasPassed)
